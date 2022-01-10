18666da04a0ab6c4315498885fa26c5d5a47cf18
Vsevolod Shmelkin <schmel924@gmail.com>
2022-01-10 09:10:08 +0300

    Gamestate in it's fetus position

--- main.lua
+++ main.lua
@@ -1,3 +1,28 @@
+--[[function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end]]
+
+function love.load()
+	GameState = {"Menu"}
+end
+
+function love.keypressed (k)
+	-- escape to previous GameState, if it's outermost - close the program
+	if k == 'escape' then
+		if GameState[1] == "Menu" then
+		    love.event.quit(0) 
+		end
+		table.remove(GameState, 1)
+	end
+
+end	
+
+function love.update(dt)
+	
+end
+
 
 function love.draw()
     love.graphics.print('Hello World!', 400, 300)
